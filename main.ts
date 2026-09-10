57b5c762e510c69a9117e81662fa7fb66c0b4da8
fanderpasaribu03-alt <fanderpasaribu03@gmail.com>
2026-09-10 09:46:11 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
-})
+namespace myRobot {
+    /**
+     * Menyalakan LED custom
+     */
+    //% block="nyalakan lampu robot"
+    export function nyalakanLampu(): void {
+        basic.showIcon(IconNames.Yes)
+    }
+
+    /**
+     * Fungsi dengan parameter angka
+     */
+    //% block="gerak maju dengan kecepatan %speed"
+    //% speed.min=0 speed.max=100
+    export function gerakMaju(speed: number): void {
+        // logika gerak motor di sini
+        basic.showNumber(speed)
+    }
+}
+myRobot.nyalakanLampu()
